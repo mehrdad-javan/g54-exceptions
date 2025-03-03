3597299c6497ba6128bc977c00e61bee06c77b9e
Mehrdad Javan <mehrdad.javan@lexicon.se>
2025-03-03 11:07:59 +0100

    catch check exceptions and read-write files + finally with try-resources block

--- src/main/java/se/lexicon/ExceptionDemo.java
+++ src/main/java/se/lexicon/ExceptionDemo.java
@@ -1,10 +1,13 @@
 package se.lexicon;
 
-import java.nio.file.Files;
-import java.nio.file.Paths;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.nio.file.*;
 import java.time.LocalDate;
 import java.time.format.DateTimeParseException;
 import java.util.InputMismatchException;
+import java.util.List;
 import java.util.Scanner;
 
 public class ExceptionDemo {
@@ -19,7 +22,10 @@ public class ExceptionDemo {
         // System.out.println(numbers[5]);
 
         // System.out.println((takeDecimalInput()));
-        System.out.println((getDate()));
+        //System.out.println((getDate()));
+        //readTextFile();
+
+        readImageAndCopy();
         System.out.println("DONE");
     }
 
@@ -59,5 +65,98 @@ public class ExceptionDemo {
 
     }
 
+    public static void readTextFile() {
+        // 1. newBufferedReader(Path path)
+        // Used for reading large files.
+        try {
+            Path path = Paths.get("dir/skills.txt");
+            BufferedReader reader = Files.newBufferedReader(path);
+            reader.lines().forEach(line -> System.out.println(line));
+            // Absolute and Relative path
+            // Absolute Path: A complete path from the root directory
+            // Specifies the full path to the file or directory
+
+            // Relative Path
+            // A path relative to the current working directory
+
+            // use absolute path when the file location is fixed.
+            // use relative path when working with files in the project directories.
+
+            System.out.println("----------------");
+            // https://docs.oracle.com/javase/8/docs/api/java/nio/file/Files.html
+            // 2. readAllLines()
+            // Suitable for small files where all lines are needed at once.
+            List<String> stringList = Files.readAllLines(path);
+            stringList.forEach(s -> System.out.println(s));
+
+        } catch (InvalidPathException e) {
+            System.out.println("Error: file path is not valid");
+        } catch (IOException e) {
+            // e.printStackTrace();
+            System.out.println("IOException: Something went wrong while reading the file");
+        } catch (Exception e) {
+            System.out.println("Unexpected Exception: call support teams and repost this issue ");
+            e.printStackTrace();
+        }
+
+    }
+
+
+    public static void readImageAndCopy() {
+        try {
+            Files.copy(
+                    Paths.get("source/java_logo.png"),
+                    Paths.get("destination/new_java_logo.png"),
+                    StandardCopyOption.REPLACE_EXISTING
+            );
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    public static void writeTextToFileWithFinallyBlock() {
+        BufferedWriter writer = null;
+        Path filePath = Paths.get("dir/skills.txt");
+
+        try {
+            writer = Files.newBufferedWriter(filePath, StandardOpenOption.CREATE, StandardOpenOption.APPEND);
+
+            writer.append("JavaScript");
+            writer.newLine();
+
+            System.out.println("Data written successfully!");
+
+        } catch (IOException e) {
+            // Handle file I/O exceptions (e.g., file not found, write failure)
+            e.printStackTrace();
+        } finally {
+            try {
+                writer.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+
+    // try-with-recourses for automatic resource management
+    public static void writeTextToFile() {
+        Path filePath = Paths.get("dir/skills.txt");
+        try (
+                BufferedWriter writer = Files.newBufferedWriter(filePath, StandardOpenOption.CREATE, StandardOpenOption.APPEND);
+        ) {
+
+            writer.append("JavaScript");
+            writer.newLine();
+
+            System.out.println("Data written successfully!");
+
+        } catch (IOException e) {
+            // Handle file I/O exceptions (e.g., file not found, write failure)
+            e.printStackTrace();
+        }
+    }
 
 }
